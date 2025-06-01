de746526f29cc47671153eca395edeb349969b99
Lemiasheusky Vladislav <vlad.lemeshok@gmail.com>
2025-06-01 12:22:20 +0300

    Delete Lab6/test.js

--- Lab6/test.js
+++
@@ -1,8 +0,0 @@
-const sendmail = require("sendmail")({ silent: true });
-
-sendmail({
-  from: "vlad.lemeshok@gmail.com",
-  to: "vlad.lemeshok@gmail.com",
-  subject: "test",
-  text: "Hello world!",
-});
